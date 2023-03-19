8d23610c84ff7b63113fdea379de7564d8395729
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2023-03-19 14:01:41 +0530

    Sopme push

--- app/src/main/java/com/example/myfirstcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstcomposeapp/MainActivity.kt
@@ -16,28 +16,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyFirstComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            NameCard("Swoyam Siddharth Nayak")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyFirstComposeAppTheme {
-        Greeting("Android")
-    }
+fun NameCard(name: String){
+    Text(text = "Hello $name")
 }
