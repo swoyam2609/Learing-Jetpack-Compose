6731bb153dac5343b0a522476f17184286251ee9
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2023-03-20 11:51:12 +0530

    Push after lecture 04

--- app/src/main/java/com/example/myfirstcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstcomposeapp/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.myfirstcomposeapp
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myfirstcomposeapp.ui.theme.MyFirstComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,16 +31,31 @@ class MainActivity : ComponentActivity() {
 data class Profile(val name: String, val description: String)
 
 @Composable
-fun ProfileCard(profile: Profile){
-    Column() {
-        Text(text = profile.name)
-        Text(text = profile.description)
-    }
-}
+fun ProfileCard(profile: Profile) {
+    Row(modifier = Modifier.padding(10.dp)) {
+        Image(
+            painter = painterResource(id = if(isSystemInDarkTheme()){R.drawable.profilepicture} else {R.drawable.profilepicturedark}),
+            contentDescription = "Profile Picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(3.dp, MaterialTheme.colors.primary, CircleShape)
+        )
 
+        Spacer(modifier = Modifier.size(4.dp))
+
+        Column() {
+            Text(text = profile.name, style = MaterialTheme.typography.h6)
+            Spacer(modifier = Modifier.height(2.dp))
+            Text(text = profile.description)
+        }
+    }
+
+}
 @Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
-fun ProfileCardPreview(){
+fun ProfileCardPreview() {
     MyFirstComposeAppTheme() {
         ProfileCard(profile = Profile("Swoyam", "Kotlin Developer"))
     }
